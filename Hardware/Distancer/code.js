98916d9b3383397a379e0a551857a56b00c2ece8
ReDoToys <account@redo.toys>
2025-11-14 12:12:25 +0100

    distancer fix

--- Hardware/Distancer/code.js
+++ Hardware/Distancer/code.js
@@ -2,15 +2,15 @@ let h = { name: 'Height', type: 'FLOAT', value: 3, constraint: 'NONZERO', unit: 
 h = 3;
 
 Begin();
-let hexShape = new Sketch([1 * 0.7, 0])
-  .LineTo([0.5 * 0.7, 0.866 * 0.7])
-  .LineTo([-0.5 * 0.7, 0.866 * 0.7])
-  .LineTo([-1 * 0.7, 0])
-  .LineTo([-0.5 * 0.7, -0.866 * 0.7])
-  .LineTo([0.5 * 0.7, -0.866 * 0.7])
+let hexShape = new Sketch([1 * 0.27, 0])
+  .LineTo([0.5 * 0.27, 0.866 * 0.27])
+  .LineTo([-0.5 * 0.27, 0.866 * 0.27])
+  .LineTo([-1 * 0.27, 0])
+  .LineTo([-0.5 * 0.27, -0.866 * 0.27])
+  .LineTo([0.5 * 0.27, -0.866 * 0.27])
   .End(true)
   .Face();
 
 let hexPrism = Extrude(hexShape, [0,0,h]);
-let cylinder = Cylinder(0.3, h, false);
+let cylinder = Cylinder(0.13, h, false);
 let distancer = Difference(hexPrism, [cylinder]);
