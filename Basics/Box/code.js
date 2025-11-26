82d88402dcac6c2c422123f987e55c89d68a9c50
ReDoToys <account@redo.toys>
2025-11-26 09:38:14 +0100

    BOX PVSM fix

--- Basics/Box/code.js
+++
@@ -1,9 +0,0 @@
-let w = { name: 'Width',  type: 'FLOAT', value: 2, constraint: 'NONZERO', unit: 'cm' };
-let h = { name: 'Height', type: 'FLOAT', value: 3, constraint: 'NONZERO', unit: 'cm' };
-let l = { name: 'Length', type: 'FLOAT', value: 4, constraint: 'NONZERO', unit: 'cm' };
-w = 2;
-h = 3;
-l = 4;
-
-Begin();
-Box(w,h,l);
